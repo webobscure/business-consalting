0c30b3b05724452129fe8fa22217bf874d3a8c54
webobscure <sparkygino@gmail.com>
2023-04-22 20:42:06 +0300

    media content

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,39 +1,42 @@
 import React from 'react';
-
+import logo from '../assets/logo.svg'
 const Header = () => {
   return (
     <>
       <header>
-        <div className="header">
-          <nav className="nav">
-            <ul>Logo</ul>
-            <ul className="nav">
-              <li className="nav-link">
-                <a href="#">Home</a>
-              </li>
-              <li className="nav-link">
-                <a href="#">About</a>
-              </li>
-              <li className="nav-link">
-                <a href="#">Pricing</a>
-              </li>
-              <li className="nav-link">
-                <a href="#">Service</a>
-              </li>
-              <li className="nav-link">
-                <a href="#">Blog</a>
-              </li>
-              <li className="nav-link">
-                <a href="#">Contact</a>
-              </li>
-            </ul>
-            <ul className="control">
-              <li>
-                <button className="btn">Register</button>
-              </li>
-            </ul>
-          </nav>
+        <div className="logo">
+          <img src={logo} alt="logo" />
         </div>
+        <div className="hamburger">
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+        </div>
+        <nav className="navbar">
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Pricing</a>
+            </li>
+            <li>
+              <a href="#">Service</a>
+            </li>
+            <li>
+              <a href="#">Blog</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+            <li>
+              <button>Register</button>
+            </li>
+          </ul>
+        </nav>
       </header>
     </>
   );
