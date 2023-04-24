7bb5f5b19f2a9e3eac61e35db7e682f53bf7e4e6
webobscure <sparkygino@gmail.com>
2023-04-24 21:38:29 +0300

    first section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,8 +5,13 @@ const Home = () => {
     <>
       <section className="first-serction">
         <div className="lead">
+          
+          <h5 className="text">
           <div className="rectangle"></div>
-          <h5 className="text">Build your plan</h5>
+          Build your plan
+          </h5>
+          <h4 className="description">Everything you need to run your online business</h4>
+          <p className="small-text">Get education on business planning, direct access to planning experts and a funding platform that’s helped raise over $500 million for startups and small businesses world-wide.</p>
         </div>
       </section>
     </>
