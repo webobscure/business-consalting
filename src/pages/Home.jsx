21c25d1905c40f10f0abaa86408a52a31dbb80f8
webobscure <sparkygino@gmail.com>
2023-04-26 23:15:52 +0300

    first section finished

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,49 @@
 import React from 'react';
 
+import person from '../assets/person.svg';
+import { FaCheckCircle, FaPlay } from 'react-icons/fa';
+
 const Home = () => {
   return (
     <>
       <section className="first-serction">
         <div className="lead">
-          
-          <h5 className="text">
-          <div className="rectangle"></div>
-          Build your plan
-          </h5>
+          <div className="lead-breef">
+            <h5 className="text">
+              <div className="rectangle"></div>
+              Build your plan
+            </h5>
+          </div>
           <h4 className="description">Everything you need to run your online business</h4>
-          <p className="small-text">Get education on business planning, direct access to planning experts and a funding platform that’s helped raise over $500 million for startups and small businesses world-wide.</p>
+          <p className="small-text">
+            Get education on business planning, direct access to planning experts and a funding
+            platform that’s helped raise over $500 million for startups and small businesses
+            world-wide.
+          </p>
+          <button className="btn">
+            Get Started
+            <label className="btn-desc"> - For Free</label>
+          </button>
+          <button className="watch">
+            <FaPlay className="player" />
+            Watch video
+          </button>
         </div>
+        <div className="lead-right">
+          <div className="red-caption">
+            <FaCheckCircle className="red-caption__check" />
+            <p className="red-caption__text">I'd be lost without Affirm.</p>
+          </div>
+          <div className="yellow-caption">
+            <FaCheckCircle className='yellow-caption__check' />
+            <p className="yellow-caption__text">
+              the best on the net!
+            </p>
+          </div>
+          <img src={person} alt="person" className="person" />
+        </div>
+        <div className="yellow-line"></div>
+
       </section>
     </>
   );
